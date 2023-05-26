a690f8be1ae9a2e523f1e510f62230d1c918ad3b
damien leduc <damien.led69@gmail.com>
2023-05-26 10:59:52 +0200

    ..

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,6 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
 
   def new
     @bookmark = Bookmark.new
@@ -7,17 +9,12 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list= List.find(params[:list_id])
-
-    if @bookmark.save
-      redirect_to list_path(@bookmark.list)
-    else
-      render :new
-    end
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@bookmark.list)
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.delete
   end
 
@@ -25,5 +22,13 @@ class BookmarksController < ApplicationController
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
